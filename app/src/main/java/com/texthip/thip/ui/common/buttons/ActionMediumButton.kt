e274b5aa5097a944d85e80ae8c6bbc554902da28
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-05-17 01:45:41 +0900

    [UI]: action medium button 컴포넌트 제작 (#17)

---
+++ app/src/main/java/com/texthip/thip/ui/common/buttons/ActionMediumButton.kt
@@ -0,0 +1,118 @@
+package com.texthip.thip.ui.common.buttons
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.texthip.thip.R
+import com.texthip.thip.ui.theme.ThipTheme.colors
+import com.texthip.thip.ui.theme.ThipTheme.typography
+
+@Composable
+fun ActionMediumButton(
+    text: String,
+    icon: Painter,
+    contentColor: Color,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(44.dp)
+            .background(color = backgroundColor, shape = RoundedCornerShape(12.dp))
+            .clickable {
+                onClick()
+            },
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
+        Icon(
+            painter = icon,
+            contentDescription = null,
+            tint = contentColor,
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = text,
+            color = contentColor,
+            style = typography.smalltitle_sb600_s16_h24,
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Icon(
+            painter = painterResource(R.drawable.ic_chevron),
+            contentDescription = null,
+            tint = contentColor,
+        )
+
+
+    }
+}
+
+@Preview
+@Composable
+private fun ActionMediumButtonPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp)
+    ) {
+        ActionMediumButton(
+            text = stringResource(R.string.add_to_my_feed),
+            icon = painterResource(R.drawable.ic_search),
+            contentColor = colors.White,
+            backgroundColor = colors.Grey02,
+            modifier = Modifier.width(180.dp),
+            onClick = {},
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        ActionMediumButton(
+            text = stringResource(R.string.add_to_my_feed),
+            icon = painterResource(R.drawable.ic_plus),
+            contentColor = colors.White,
+            backgroundColor = colors.Purple,
+            modifier = Modifier.width(180.dp),
+            onClick = {},
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        ActionMediumButton(
+            text = stringResource(R.string.add_to_my_feed),
+            icon = painterResource(R.drawable.ic_search),
+            contentColor = colors.Grey,
+            backgroundColor = Color.Transparent,
+            modifier = Modifier.width(180.dp)
+                .border(width = 1.dp, color = colors.Grey02, shape = RoundedCornerShape(12.dp)),
+            onClick = {},
+        )
+    }
+}
