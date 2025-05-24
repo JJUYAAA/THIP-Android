d52c6ef2bd9f8cdec74c355e1053707bcde327ef
juyeon <jjjjyy020704@naver.com>
2025-05-24 14:45:54 +0900

    [ui]: view 컴포넌트 생성(#19)

---
+++ app/src/main/java/com/texthip/thip/ui/common/view/CountingBar.kt
@@ -0,0 +1,56 @@
+package com.texthip.thip.ui.common.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.texthip.thip.R
+
+@Composable
+fun CountingBar(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .background(colorResource(id = R.color.black), shape = RoundedCornerShape(20.dp))
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = text,
+                //color = colorResource(id = R.color.teal_200),
+                color = Color(0xFFAA7FFB4),
+                fontSize = 14.sp
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CountingBarPrev() {
+    CountingBar(
+        text = stringResource(id = R.string.counting_bar_text),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    )
+}
