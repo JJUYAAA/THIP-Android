b9478bed327996a7c1c273587aeed6e7e5aedeba
Gyubin <rbqks529@naver.com>
2025-06-28 01:28:53 +0900

    [UI] CardItemRoomSmall 추가 (#25)

---
+++ app/src/main/java/com/texthip/thip/ui/common/cards/CardItemRoomSmall.kt
@@ -0,0 +1,133 @@
+package com.texthip.thip.ui.common.cards
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.texthip.thip.R
+import com.texthip.thip.ui.theme.ThipTheme.colors
+import com.texthip.thip.ui.theme.ThipTheme.typography
+
+@Composable
+fun CardItemRoomSmall(
+    modifier: Modifier = Modifier,
+    title: String,
+    participants: Int,
+    maxParticipants: Int,
+    endDate: Int, // 남은 일 수 (예: 3)
+    imageRes: Int? = R.drawable.bookcover_sample_small,
+    onClick: () -> Unit = {}
+) {
+    Card(
+        modifier = modifier
+            .size(width = 232.dp, height = 104.dp)
+            .clickable { onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = colors.DarkGrey50
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // 이미지
+                Box(
+                    modifier = Modifier
+                        .size(width = 60.dp, height = 80.dp)
+                ) {
+                    imageRes?.let {
+                        Image(
+                            painter = painterResource(id = it),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = title,
+                        color = colors.White,
+                        style = typography.menu_sb600_s14_h24,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            painter = painterResource(id = R.drawable.ic_group),
+                            contentDescription = "그룹 아이콘",
+                            tint = Color.White
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+
+                        Text (
+                            text = "$participants / ${maxParticipants}명",
+                            color = colors.White,
+                            style = typography.info_m500_s12
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "${endDate}일 뒤 모집 마감",
+                        color = colors.Red,
+                        style = typography.menu_sb600_s12_h20
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000, widthDp = 360)
+@Composable
+fun CardItemRoomSmallPreview() {
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+        CardItemRoomSmall(
+            title = "방 제목입니다 방 제목입니다",
+            participants = 22,
+            maxParticipants = 30,
+            endDate = 3,
+            imageRes = R.drawable.bookcover_sample
+        )
+
+    }
+}
--- app/src/main/java/com/texthip/thip/ui/myPage/myGroup/MyGroupScreen.kt
+++ app/src/main/java/com/texthip/thip/ui/myPage/myGroup/MyGroupScreen.kt
@@ -1,36 +1,51 @@
 package com.texthip.thip.ui.myPage.myGroup
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.texthip.thip.ui.common.cards.CardItemRoom
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.texthip.thip.R
-import com.texthip.thip.ui.common.cards.CardItemRoom
 import com.texthip.thip.ui.theme.ThipTheme.colors
 import com.texthip.thip.ui.theme.ThipTheme.typography
 import androidx.compose.foundation.lazy.items
 
 @Composable
-fun MyGroupListScreen(
-    dataList: List<CardItemRoomData>,
+fun MyGroupScreen(
+    allDataList: List<CardItemRoomData>,
     onCardClick: (CardItemRoomData) -> Unit = {}
 ) {
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    // 필터링
+    val filteredList = remember(selectedIndex, allDataList) {
+        if (selectedIndex == 0) {
+            allDataList.filter { !it.isRecruiting }
+        } else {
+            allDataList.filter { it.isRecruiting }
+        }
+    }
+
     Column(
         Modifier
             .background(colors.Black)
@@ -38,8 +53,7 @@ fun MyGroupListScreen(
             .padding(horizontal = 20.dp)
     ) {
         Spacer(modifier = Modifier.height(16.dp))
-
-        // 상단 타이틀 (필요 시 아이콘 등 추가)
+        // 상단 타이틀
         Row(
             Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
@@ -58,12 +72,16 @@ fun MyGroupListScreen(
         }
         Spacer(modifier = Modifier.height(20.dp))
 
+        FilterRow(selectedIndex = selectedIndex, onSelect = { selectedIndex = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+
         // 리스트
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(20.dp),
             modifier = Modifier.fillMaxSize()
         ) {
-            items(dataList) { item ->
+            items(filteredList) { item ->
                 CardItemRoom(
                     title = item.title,
                     participants = item.participants,
@@ -78,16 +96,31 @@ fun MyGroupListScreen(
     }
 }
 
+
 @Preview(showBackground = true, backgroundColor = 0xFF000000, widthDp = 360, heightDp = 800)
 @Composable
-fun MyGroupListScreenPreview() {
-    val dataList = mutableListOf(
+fun MyGroupListFilterScreenPreview() {
+    val dataList = listOf(
         CardItemRoomData(
             title = "모임방 이름입니다. 모임방...",
             participants = 22,
             maxParticipants = 30,
             isRecruiting = true,
             endDate = 3
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = false,
+            endDate = 30
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = true,
+            endDate = 1
         ),
         CardItemRoomData(
             title = "모임방 이름입니다. 모임방...",
@@ -101,8 +134,57 @@ fun MyGroupListScreenPreview() {
             participants = 22,
             maxParticipants = 30,
             isRecruiting = true,
+            endDate = 3
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = false,
+            endDate = 30
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = true,
             endDate = 1
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = false,
+            endDate = 3
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = true,
+            endDate = 3
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = false,
+            endDate = 30
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = true,
+            endDate = 1
+        ),
+        CardItemRoomData(
+            title = "모임방 이름입니다. 모임방...",
+            participants = 22,
+            maxParticipants = 30,
+            isRecruiting = false,
+            endDate = 3
         )
     )
-    MyGroupListScreen(dataList)
+    MyGroupScreen(allDataList = dataList)
 }
