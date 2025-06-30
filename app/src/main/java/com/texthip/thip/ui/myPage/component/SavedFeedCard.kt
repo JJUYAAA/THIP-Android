3e9b1423c5576688cba18ec5bbe0ed5c47fa2dcf
juyeon <jjjjyy020704@naver.com>
2025-07-01 03:15:44 +0900

    [chore]: mypage.component 패키지 생성(#26)

--- app/src/main/java/com/texthip/thip/ui/myPage/component/SavedFeedCard.kt
+++ app/src/main/java/com/texthip/thip/ui/myPage/component/SavedFeedCard.kt
@@ -1,17 +1,13 @@
-package com.texthip.thip.ui.myPage
+package com.texthip.thip.ui.myPage.component
 
-import android.R.attr.height
-import android.R.attr.top
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
