c3e106cae43fb2249bb254291d814a31b6946b68
juyeon <jjjjyy020704@naver.com>
2025-07-01 04:02:41 +0900

    [ui]: ReactionScreen 버튼 컴포넌트 변경(#26)

--- app/src/main/java/com/texthip/thip/ui/common/cards/CardAlarm.kt
+++ app/src/main/java/com/texthip/thip/ui/common/cards/CardAlarm.kt
@@ -25,6 +25,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
--- app/src/main/java/com/texthip/thip/ui/myPage/ReactionsScreen.kt
+++ app/src/main/java/com/texthip/thip/ui/myPage/ReactionsScreen.kt
@@ -11,24 +11,29 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.texthip.thip.R
 import com.texthip.thip.ui.common.buttons.HeaderButton
+import com.texthip.thip.ui.common.buttons.OptionChipButton
 import com.texthip.thip.ui.common.cards.NotificationCard
 import com.texthip.thip.ui.common.topappbar.DefaultTopAppBar
-import com.texthip.thip.ui.myPage.data.ReactionItem
+import com.texthip.thip.ui.myPage.mock.ReactionItem
 import com.texthip.thip.ui.theme.Black
 
 @Composable
 fun ReactionsScreen() {
     //추후 뷰모델로 수정 예정
     val reactions = listOf(
-        ReactionItem("@user 1님의 피드 글","어쩌구 저쩌구 콘텐츠 내용입니다.어쩌구 저쩌구 콘텐츠 내용입니다.어쩌구 저쩌구 콘텐츠 내용입니다.", "2",false),
+        ReactionItem("@user 1님의 피드 글","어쩌구 저쩌구 콘텐츠 내용입니다.어쩌구 저쩌구 콘텐츠 내용입니다.어쩌구 저쩌구 콘텐츠 내용입니다.", "2",true),
         ReactionItem("@user 1님의 글", "어쩌구 저쩌구 콘텐츠 내용입니다.", "7", true),
-        ReactionItem("@user 3님의 기록", "어쩌구 저쩌구 콘텐츠 내용입니다.", "17", false),
+        ReactionItem("@user 3님의 기록", "어쩌구 저쩌구 콘텐츠 내용입니다.", "17", true),
         ReactionItem("@user 1님의 글", "어쩌구 저쩌구 콘텐츠 내용입니다.", "7", true),
         ReactionItem("@user 1님의 글", "어쩌구 저쩌구 콘텐츠 내용입니다.", "7", true),
         ReactionItem("@user 1님의 글", "어쩌구 저쩌구 콘텐츠 내용입니다.", "7", true),
@@ -39,6 +44,8 @@ fun ReactionsScreen() {
         ReactionItem("@user 1님의 글", "어쩌구 저쩌구 콘텐츠 내용입니다.", "7", true),
         ReactionItem("@user 1님의 글", "어쩌구 저쩌구 콘텐츠 내용입니다.", "7", true)
     )
+    var isLikesSelected by remember { mutableStateOf(false) }
+    var isCommentsSelected by remember { mutableStateOf(false) }
     Scaffold(
         containerColor = Black,
         topBar = {
@@ -53,22 +60,28 @@ fun ReactionsScreen() {
                 .padding(innerPadding)
                 .fillMaxSize()
         ) {
+            //TODO card 컴포넌트 수정 후 적용 & 필터링 기능
             Row(modifier = Modifier.padding(start = 20.dp, top = 20.dp, bottom = 20.dp)) {
-                HeaderButton(
+                OptionChipButton(
                     text = stringResource(R.string.likes),
-                    enabled = false
+                    isFilled = true,
+                    onClick = {
+                    }
                 )
                 Spacer(modifier = Modifier.width(12.dp))
-                HeaderButton(
+                OptionChipButton(
                     text = stringResource(R.string.comments),
-                    enabled = false
+                    isFilled = true,
+                    onClick = {
+                    }
                 )
+
             }
             LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(horizontal = 20.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp)
+                verticalArrangement = Arrangement.spacedBy(20.dp)
             ) {
                 items(reactions) { item ->
                     NotificationCard(
