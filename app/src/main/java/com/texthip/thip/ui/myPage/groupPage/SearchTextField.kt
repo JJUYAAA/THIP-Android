77a563e477415f475ac01c4ffb13b34db7ea2905
JoGyuBin <128724038+rbqks529@users.noreply.github.com>
2025-06-30 16:15:46 +0900

    [UI] GrpupPageScreen 임시 TopBar 추가 및 검색 component 구현 (#25)

---
+++ app/src/main/java/com/texthip/thip/ui/myPage/groupPage/SearchTextField.kt
@@ -0,0 +1,79 @@
+package com.texthip.thip.ui.myPage.groupPage
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.texthip.thip.R
+import com.texthip.thip.ui.theme.ThipTheme.colors
+import com.texthip.thip.ui.theme.ThipTheme.typography
+
+@Composable
+fun SearchTextField(
+    onValueChange: (String) -> Unit
+) {
+    var value by rememberSaveable { mutableStateOf("") }
+
+    Box(
+        Modifier
+            .padding(horizontal = 20.dp)
+            .fillMaxWidth()
+            .height(48.dp)
+    ) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .fillMaxWidth(),
+            placeholder = {
+                Text(
+                    text = "모임방 참여할 사람!",
+                    color = colors.Grey02,
+                    style = typography.menu_r400_s14_h24.copy(lineHeight = 2.sp)
+                )
+            },
+            singleLine = true,
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.colors(
+                focusedTextColor = colors.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedContainerColor = colors.DarkGrey,
+                unfocusedContainerColor = colors.DarkGrey,
+                cursorColor = colors.NeonGreen
+            ),
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_search),
+                    contentDescription = "검색",
+                    tint = colors.White,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF000000, widthDp = 360)
+@Composable
+fun PreviewSearchTextField() {
+    SearchTextField(onValueChange = {})
+}
+
