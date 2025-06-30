c2eea1b4ab5be24e5bf572562066925bcf63f182
juyeon <jjjjyy020704@naver.com>
2025-06-30 23:03:03 +0900

    [ui]: SavedFeedCard 컴포넌트 수정(#26)

--- app/src/main/java/com/texthip/thip/ui/myPage/SavedFeedCard.kt
+++ app/src/main/java/com/texthip/thip/ui/myPage/SavedFeedCard.kt
@@ -1,5 +1,6 @@
 package com.texthip.thip.ui.myPage
 
+import android.R.attr.height
 import android.R.attr.top
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -8,13 +9,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -62,13 +66,15 @@ fun SavedFeedCard(
             bookAuthor = auth_name,
             onClick = {}
         )
+        Spacer(modifier = Modifier.height(16.dp))
         if (imageRes != null) {
             Image(
                 painter = imageRes,
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 16.dp)
+                    .height(480.dp),
+                contentScale = ContentScale.Crop
             )
         }
 
