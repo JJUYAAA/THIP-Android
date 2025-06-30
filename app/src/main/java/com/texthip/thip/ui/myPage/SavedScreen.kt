570459535f4320c4ee0e1bc7a313e217909c948c
juyeon <jjjjyy020704@naver.com>
2025-06-30 16:51:18 +0900

    [ui]: 저장 페이지 - BookContent까지 구현(#26)

---
+++ app/src/main/java/com/texthip/thip/ui/myPage/SavedScreen.kt
@@ -0,0 +1,140 @@
+package com.texthip.thip.ui.myPage.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.texthip.thip.R
+import com.texthip.thip.ui.common.cards.CardBookList
+import com.texthip.thip.ui.common.topappbar.DefaultTopAppBar
+import com.texthip.thip.ui.myPage.viewmodel.SavedBookViewModel
+import com.texthip.thip.ui.theme.Black
+import com.texthip.thip.ui.theme.Grey02
+import com.texthip.thip.ui.theme.ThipTheme.typography
+import com.texthip.thip.ui.theme.White
+
+@Composable
+fun SavedScreen() {
+    val tabs = listOf("피드", "책")
+    var selectedTabIndex by rememberSaveable { mutableStateOf(0) }
+
+    Scaffold(
+        containerColor = Black,
+        topBar = {
+            DefaultTopAppBar(
+                title = stringResource(R.string.saved),
+                onLeftClick = {},
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Box(modifier = Modifier.width(160.dp)) {
+                TabRow(
+                    selectedTabIndex = selectedTabIndex,
+                    containerColor = Color.Transparent,
+                    contentColor = White,
+                    indicator = { tabPositions ->
+                        val tabPosition = tabPositions[selectedTabIndex]
+
+                        Box(
+                            modifier = Modifier
+                                .tabIndicatorOffset(tabPosition)
+                                .width(10.dp)
+                                .height(1.dp)
+                                .align(Alignment.BottomCenter)
+                                .clip(RoundedCornerShape(1.5.dp))
+                                .background(White)
+                        )
+                    },
+                    divider = {}) {
+
+                    tabs.forEachIndexed { index, title ->
+                        val selected = selectedTabIndex == index
+                        Tab(
+                            modifier = Modifier.width(80.dp),
+                            selected = selected,
+                            onClick = { selectedTabIndex = index },
+                            selectedContentColor = White,
+                            unselectedContentColor = Grey02,
+                            text = {
+                                Box(
+                                    modifier = Modifier.width(60.dp),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = title,
+                                        style = typography.smalltitle_sb600_s18_h24
+                                    )
+                                }
+                            }
+                        )
+                    }
+                }
+            }
+            Box(modifier = Modifier.fillMaxWidth()) {
+                when (selectedTabIndex) {
+                    0 -> FeedContent()
+                    1 -> BookContent()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FeedContent() {
+    Text("피드 항목들 보여주는 Composable")
+}
+
+@Composable
+fun BookContent(viewModel: SavedBookViewModel = viewModel()) {
+    val books = viewModel.bookList
+
+    LazyColumn {
+        items(items = books, key = { it.id }) { book ->
+            CardBookList(
+                title = book.title,
+                author = book.author,
+                imageRes = null,
+                publisher = book.publisher,
+                isBookmarked = book.isSaved,
+                onBookmarkClick = { viewModel.toggleBookmark(book.id) }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SavedScreenPrev() {
+    SavedScreen()
+}
