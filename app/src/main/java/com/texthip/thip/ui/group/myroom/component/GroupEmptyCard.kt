6ec09d2a584aeae582ede42723387a835dd34138
Gyubin <rbqks529@naver.com>
2025-07-12 18:15:34 +0900

    [ui]: GroupPager의 예외처리 화면 추가 (#44)

---
+++ app/src/main/java/com/texthip/thip/ui/group/myroom/component/GroupEmptyCard.kt
@@ -0,0 +1,118 @@
+package com.texthip.thip.ui.group.myroom.component
+
+import android.R.attr.data
+import android.R.attr.top
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.texthip.thip.R
+import com.texthip.thip.ui.group.myroom.mock.GroupCardData
+import com.texthip.thip.ui.theme.ThipTheme
+import com.texthip.thip.ui.theme.ThipTheme.colors
+import com.texthip.thip.ui.theme.ThipTheme.typography
+
+@Composable
+fun GroupEmptyCard(
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = Color.White,
+    onClick: () -> Unit = {}
+) {
+    // 그라데이션
+    val gradient = Brush.linearGradient(
+        colors = listOf(
+            colors.White,
+            colors.Grey01
+        ),
+        start = Offset(0f, 0f),
+        end = Offset(1000f, 1000f)
+    )
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(176.dp)
+            .clickable { onClick() },
+        shape = RoundedCornerShape(18.dp),
+        colors = CardDefaults.cardColors(containerColor = backgroundColor),
+        elevation = CardDefaults.cardElevation(defaultElevation = 0.dp)
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(brush = gradient)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 34.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween // 위, 아래 끝에 배치
+            ) {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(R.string.group_empty_card_title),
+                        style = typography.smalltitle_sb600_s18_h24,
+                        color = colors.Black
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    Text(
+                        text = stringResource(R.string.group_empty_card_description),
+                        style = typography.copy_r400_s14,
+                        color = colors.Grey02
+                    )
+                }
+
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_thip_empty_room),
+                    contentDescription = "Empty Group Icon",
+                    tint = Color.Unspecified
+                )
+            }
+        }
+    }
+}
+
+
+@Preview()
+@Composable
+fun GroupEmptyCardPreview() {
+    ThipTheme {
+        GroupEmptyCard(
+            onClick = {}
+        )
+    }
+}
