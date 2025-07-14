5a5109f70ff6d1b4d10943855756eb71f7f4e306
JJUYAAA <jjjjyy020704@naver.com>
2025-07-14 15:42:19 +0900

    [ui]: 내피드_프로필 헤더 추가 (#45)

--- app/src/main/java/com/texthip/thip/ui/common/header/AuthorHeader.kt
+++ app/src/main/java/com/texthip/thip/ui/common/header/AuthorHeader.kt
@@ -2,8 +2,6 @@ package com.texthip.thip.ui.common.header
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,29 +12,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.texthip.thip.ui.common.buttons.OutlinedButton
 import com.texthip.thip.ui.theme.ThipTheme
 import com.texthip.thip.ui.theme.ThipTheme.colors
 import com.texthip.thip.ui.theme.ThipTheme.typography
-import com.texthip.thip.R
-import com.texthip.thip.ui.common.buttons.OutlinedButton
-import com.texthip.thip.ui.theme.Grey02
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun AuthorHeader(
     modifier: Modifier = Modifier,
-    profileImage: Painter?,
+    profileImage: String?,
     nickname: String,
     badgeText: String,
     buttonText: String = "",
@@ -52,7 +45,7 @@ fun AuthorHeader(
     ) {
         if (profileImage != null) {
             Image(
-                painter = profileImage,
+                painter = rememberAsyncImagePainter(profileImage),
                 contentDescription = "작성자 장르이미지",
                 modifier = Modifier
                     .size(54.dp)
--- app/src/main/java/com/texthip/thip/ui/feed/screen/FeedScreen.kt
+++ app/src/main/java/com/texthip/thip/ui/feed/screen/FeedScreen.kt
@@ -1,42 +1,70 @@
 package com.texthip.thip.ui.feed.screen
 
+import android.widget.Space
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.texthip.thip.R
-import com.texthip.thip.ui.common.topappbar.LeftNameTopAppBar
+import com.texthip.thip.ui.common.header.AuthorHeader
+import com.texthip.thip.ui.common.header.HeaderMenuBarTab
 import com.texthip.thip.ui.common.topappbar.LogoTopAppBar
+import com.texthip.thip.ui.theme.ThipTheme
 import com.texthip.thip.ui.theme.ThipTheme.colors
 
 @Composable
-fun FeedScreen(navController: NavController) {
+fun FeedScreen(navController: NavController? = null,
+               nickname: String,
+               userRole: String) {
+    var selectedIndex = rememberSaveable { mutableIntStateOf(0) }
+
     Column(
         Modifier
-            .background(colors.Black)
             .fillMaxSize()
     ) {
         LogoTopAppBar(
             leftIcon = painterResource(R.drawable.ic_plusfriend),
-            hasNotification = true,
+            hasNotification = false,
             onLeftClick = {},
             onRightClick = {}
         )
+        Spacer(modifier = Modifier.height(40.dp))
+        HeaderMenuBarTab(
+            titles = listOf("피드", "내 피드"),
+            selectedTabIndex = selectedIndex.value,
+            onTabSelected = { selectedIndex.value = it }
+        )
         Column(
             modifier = Modifier
-                .padding(horizontal = 20.dp)
                 .fillMaxSize()
         ) {
-
+            Spacer(modifier = Modifier.height(32.dp))
+            AuthorHeader(
+                profileImage = null,
+                nickname = nickname,
+                badgeText = userRole,
+                buttonText = "",
+                buttonWidth = 60.dp,
+                showButton = false
+            )
         }
+    }
+}
+
+@Preview
+@Composable
+private fun FeesScreenPrev() {
+    ThipTheme {
+        FeedScreen(nickname = "ThipUser01", userRole = "문학 칭호")
     }
 }
