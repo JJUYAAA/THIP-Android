15964b4af67807032dae65662bdae312bf3cb8bd
rbqks529 <rbqks529@naver.com>
2025-07-16 21:13:48 +0900

    [refactor]: LazyColumn으로 수정 (#44)

--- app/src/main/java/com/texthip/thip/ui/group/myroom/screen/GroupFilteredSearchResultScreen.kt
+++ app/src/main/java/com/texthip/thip/ui/group/myroom/screen/GroupFilteredSearchResultScreen.kt
@@ -2,13 +2,14 @@ package com.texthip.thip.ui.group.myroom.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -62,8 +63,10 @@ fun GroupFilteredSearchResultScreen(
             subText = stringResource(R.string.group_no_search_result2)
         )
     } else {
-        Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-            roomList.forEach { room ->
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(roomList) { room ->
                 CardItemRoomSmall(
                     title = room.title,
                     participants = room.participants,
--- app/src/main/java/com/texthip/thip/ui/group/myroom/screen/GroupLiveSearchResultScreen.kt
+++ app/src/main/java/com/texthip/thip/ui/group/myroom/screen/GroupLiveSearchResultScreen.kt
@@ -2,24 +2,26 @@ package com.texthip.thip.ui.group.myroom.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.texthip.thip.ui.common.cards.CardItemRoomSmall
 import com.texthip.thip.ui.group.myroom.mock.GroupCardItemRoomData
-import com.texthip.thip.ui.theme.ThipTheme
+import com.texthip.thip.ui.theme.ThipTheme.colors
 
 @Composable
 fun GroupLiveSearchResultScreen(
     roomList: List<GroupCardItemRoomData>
 ) {
-    val colors = ThipTheme.colors
-    Column(verticalArrangement = Arrangement.spacedBy(16.dp)) {
-        roomList.forEach { room ->
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(roomList) { room ->
             CardItemRoomSmall(
                 title = room.title,
                 participants = room.participants,
