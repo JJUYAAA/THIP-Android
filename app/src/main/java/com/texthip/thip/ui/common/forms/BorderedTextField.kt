d161fc05fd8d0fc2bfabf1727d1339d5d9898fc8
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-07-17 15:42:13 +0900

    [refactor]: bordered text field 아이콘과 기능 요구사항에 맞게 수정 (#34)

--- app/src/main/java/com/texthip/thip/ui/common/forms/BorderedTextField.kt
+++ app/src/main/java/com/texthip/thip/ui/common/forms/BorderedTextField.kt
@@ -1,6 +1,8 @@
 package com.texthip.thip.ui.common.forms
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,6 +13,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -29,10 +32,32 @@ fun BorderedTextField(
     modifier: Modifier = Modifier,
     hint: String,
     text: String,
+    canRemove: Boolean = true,
     onTextChange: (String) -> Unit,
     onDelete: () -> Unit,
+    onRemoveField: () -> Unit
 ) {
     val myStyle = typography.menu_r400_s14_h24.copy(lineHeight = 14.sp)
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+
+    val iconRes = when {
+        isFocused && text.isEmpty() -> R.drawable.ic_x_circle_darkgrey
+        isFocused && text.isNotEmpty() -> R.drawable.ic_x_circle_grey
+        !isFocused && canRemove -> R.drawable.ic_delete
+        else -> null
+    }
+
+    val iconEnabled = when (iconRes) {
+        R.drawable.ic_x_circle_grey -> true
+        R.drawable.ic_delete -> true
+        else -> false
+    }
+
+    val iconTint = when (iconRes) {
+        R.drawable.ic_delete -> colors.Grey02
+        else -> Color.Unspecified
+    }
 
     OutlinedTextField(
         value = text,
@@ -49,28 +74,32 @@ fun BorderedTextField(
             .fillMaxWidth()
             .height(48.dp),
         shape = RoundedCornerShape(12.dp),
+        interactionSource = interactionSource,
         colors = TextFieldDefaults.colors(
             focusedTextColor = colors.White,
+            unfocusedTextColor = colors.White,
             focusedIndicatorColor = colors.Grey02,
             unfocusedIndicatorColor = colors.Grey02,
             focusedContainerColor = colors.Black00,
             unfocusedContainerColor = colors.Black00,
             cursorColor = colors.NeonGreen,
         ),
-
         trailingIcon = {
-            Icon(
-                painter = painterResource(
-                    if (text.isEmpty()) R.drawable.ic_x_circle_darkgrey
-                    else R.drawable.ic_x_circle_grey
-                ),
-                contentDescription = "Clear",
-                modifier = Modifier.clickable {
-                    onDelete()
-                },
-                tint = Color.Unspecified
-            )
-        }
+            iconRes?.let {
+                Icon(
+                    painter = painterResource(id = it),
+                    contentDescription = "Delete or Clear",
+                    modifier = Modifier.clickable(enabled = iconEnabled) {
+                        when (it) {
+                            R.drawable.ic_x_circle_grey -> onDelete()
+                            R.drawable.ic_delete -> onRemoveField()
+                        }
+                    },
+                    tint = iconTint
+                )
+            }
+        },
+        singleLine = true
     )
 }
 
@@ -88,6 +117,9 @@ fun BorderedTextFieldPreview() {
             },
             onDelete = {
                 text = ""
+            },
+            onRemoveField = {
+                // 필드 제거
             },
         )
     }
