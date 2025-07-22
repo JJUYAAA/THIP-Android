8955792ca30a2044932fac82096960e3c9f066e1
JJUYAAA <jjjjyy020704@naver.com>
2025-07-22 18:21:47 +0900

    [ui]: 새 글 작성 페이지 - 바텀시트, 다이얼로그 (#45)

--- app/src/main/java/com/texthip/thip/ui/feed/screen/FeedWriteScreen.kt
+++ app/src/main/java/com/texthip/thip/ui/feed/screen/FeedWriteScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -36,8 +35,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
