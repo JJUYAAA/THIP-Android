658e813bcb49dc84946d7b016ffc475af232ed50
rbqks529 <rbqks529@naver.com>
2025-07-23 17:26:36 +0900

    [refactor]: preview 생성 (#53)

--- app/src/main/java/com/texthip/thip/ui/group/myroom/component/GroupLiveSearchResult.kt
+++ app/src/main/java/com/texthip/thip/ui/group/myroom/component/GroupLiveSearchResult.kt
@@ -2,16 +2,21 @@ package com.texthip.thip.ui.group.myroom.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.texthip.thip.R
 import com.texthip.thip.ui.common.cards.CardItemRoomSmall
 import com.texthip.thip.ui.group.myroom.mock.GroupCardItemRoomData
+import com.texthip.thip.ui.theme.ThipTheme
 import com.texthip.thip.ui.theme.ThipTheme.colors
 
 @Composable
@@ -39,6 +44,53 @@ fun GroupLiveSearchResult(
                         .background(colors.DarkGrey02)
                 )
             }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GroupLiveSearchResultPreview() {
+    ThipTheme {
+        Box(
+            modifier = Modifier
+                .background(colors.Black)
+                .padding(16.dp)
+        ) {
+            GroupLiveSearchResult(
+                roomList = listOf(
+                    GroupCardItemRoomData(
+                        title = "해리포터 독서모임",
+                        participants = 5,
+                        maxParticipants = 10,
+                        isRecruiting = true,
+                        endDate = 7,
+                        imageRes = R.drawable.bookcover_sample,
+                        genreIndex = 0,
+                        isSecret = false
+                    ),
+                    GroupCardItemRoomData(
+                        title = "소설 읽기 모임",
+                        participants = 8,
+                        maxParticipants = 12,
+                        isRecruiting = false,  
+                        endDate = 3,
+                        imageRes = R.drawable.bookcover_sample,
+                        genreIndex = 1,
+                        isSecret = true
+                    ),
+                    GroupCardItemRoomData(
+                        title = "비즈니스 서적 스터디",
+                        participants = 3,
+                        maxParticipants = 8,
+                        isRecruiting = true,
+                        endDate = null,
+                        imageRes = R.drawable.bookcover_sample,
+                        genreIndex = 2,
+                        isSecret = false
+                    )
+                )
+            )
         }
     }
 }
