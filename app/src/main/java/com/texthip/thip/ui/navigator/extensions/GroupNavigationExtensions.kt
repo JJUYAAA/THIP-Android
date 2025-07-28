7e40247a338c80d171e99cc70294c2348da792bc
rbqks529 <rbqks529@naver.com>
2025-07-28 17:40:34 +0900

    [chore]: 주석 추가 (#58)

--- app/src/main/java/com/texthip/thip/ui/navigator/extensions/GroupNavigationExtensions.kt
+++ app/src/main/java/com/texthip/thip/ui/navigator/extensions/GroupNavigationExtensions.kt
@@ -1,9 +1,9 @@
 package com.texthip.thip.ui.navigator.extensions
 
 import androidx.navigation.NavHostController
-import com.texthip.thip.ui.navigator.routes.MainTabRoutes
-import com.texthip.thip.ui.navigator.routes.GroupRoutes
 import com.texthip.thip.ui.navigator.routes.CommonRoutes
+import com.texthip.thip.ui.navigator.routes.GroupRoutes
+import com.texthip.thip.ui.navigator.routes.MainTabRoutes
 
 
 // Group 관련 네비게이션 확장 함수들
@@ -11,30 +11,39 @@ fun NavHostController.navigateToGroup() {
     navigate(MainTabRoutes.Group)
 }
 
+// 모임방 만들기 화면으로 이동
 fun NavHostController.navigateToGroupMakeRoom() {
     navigate(GroupRoutes.MakeRoom)
 }
 
+// 완료된 모임방 목록으로 이동
 fun NavHostController.navigateToGroupDone() {
     navigate(GroupRoutes.Done)
 }
 
+// 모임방 검색 화면으로 이동
 fun NavHostController.navigateToGroupSearch() {
     navigate(GroupRoutes.Search)
 }
 
+// 내 모임방 화면으로 이동
 fun NavHostController.navigateToGroupMy() {
     navigate(GroupRoutes.My)
 }
 
+// 알람 화면으로 이동
 fun NavHostController.navigateToAlarm() {
     navigate(CommonRoutes.Alarm)
 }
 
+// 모집중인 모임방 상세 화면으로 이동
 fun NavHostController.navigateToGroupRecruit(roomId: Int) {
     navigate(GroupRoutes.Recruit(roomId))
 }
 
+// 진행중인 모임방 화면으로 이동
 fun NavHostController.navigateToGroupRoom(roomId: Int) {
     navigate(GroupRoutes.Room(roomId))
 }
+
+
