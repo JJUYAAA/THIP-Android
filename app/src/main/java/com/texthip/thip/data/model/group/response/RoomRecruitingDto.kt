c8b900dba3e50913be0758480ea39ccafaab5b09
rbqks529 <rbqks529@naver.com>
2025-08-05 16:23:50 +0900

    [feat]: 모집중인 모임방 dto 생성(#65)

---
+++ app/src/main/java/com/texthip/thip/data/model/group/response/RoomRecruitingDto.kt
@@ -0,0 +1,36 @@
+package com.texthip.thip.data.model.group.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RoomRecruitingDto(
+    @SerialName("isHost") val isHost: Boolean,
+    @SerialName("isJoining") val isJoining: Boolean,
+    @SerialName("roomId") val roomId: Int,
+    @SerialName("roomName") val roomName: String,
+    @SerialName("roomImageUrl") val roomImageUrl: String?,
+    @SerialName("isPublic") val isPublic: Boolean,
+    @SerialName("progressStartDate") val progressStartDate: String,
+    @SerialName("progressEndDate") val progressEndDate: String,
+    @SerialName("recruitEndDate") val recruitEndDate: String,
+    @SerialName("category") val category: String,
+    @SerialName("roomDescription") val roomDescription: String,
+    @SerialName("memberCount") val memberCount: Int,
+    @SerialName("recruitCount") val recruitCount: Int,
+    @SerialName("isbn") val isbn: String,
+    @SerialName("bookImageUrl") val bookImageUrl: String,
+    @SerialName("bookTitle") val bookTitle: String,
+    @SerialName("authorName") val authorName: String,
+    @SerialName("bookDescription") val bookDescription: String,
+    @SerialName("recommandRooms") val recommendRooms: List<RecommendRoomDto>
+)
+
+@Serializable
+data class RecommendRoomDto(
+    @SerialName("roomImageUrl") val roomImageUrl: String?,
+    @SerialName("roomName") val roomName: String,
+    @SerialName("memberCount") val memberCount: Int,
+    @SerialName("recruitCount") val recruitCount: Int,
+    @SerialName("recruitEndDate") val recruitEndDate: String
+)
