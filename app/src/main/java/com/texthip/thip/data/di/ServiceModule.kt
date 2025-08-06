184c9ede22b98ac921ce4a63b163f5e018221ff9
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-08-06 23:11:48 +0900

    [feat]: 진행중인 방 상세보기 service module 작성 (#66)

--- app/src/main/java/com/texthip/thip/data/di/ServiceModule.kt
+++ app/src/main/java/com/texthip/thip/data/di/ServiceModule.kt
@@ -1,12 +1,18 @@
 package com.texthip.thip.data.di
 
+import com.texthip.thip.data.service.RoomsService
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ServiceModule {
-//    @Provides
-//    @Singleton
+    @Provides
+    @Singleton
+    fun providesRoomsService(retrofit: Retrofit): RoomsService =
+        retrofit.create(RoomsService::class.java)
 }
