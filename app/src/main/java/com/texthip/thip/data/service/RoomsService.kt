8cde61021c71d752e68b9cb7d22e9417d6b2ed49
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-08-07 18:35:38 +0900

    [feat]: 독서메이트 service 작성 (#66)

--- app/src/main/java/com/texthip/thip/data/service/RoomsService.kt
+++ app/src/main/java/com/texthip/thip/data/service/RoomsService.kt
@@ -2,6 +2,7 @@ package com.texthip.thip.data.service
 
 import RoomsPlayingResponse
 import com.texthip.thip.data.model.base.BaseResponse
+import com.texthip.thip.data.model.rooms.response.RoomsUsersResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -10,4 +11,9 @@ interface RoomsService {
     suspend fun getRoomsPlaying(
         @Path("roomId") roomId: Int
     ): BaseResponse<RoomsPlayingResponse>
+
+    @GET("rooms/{roomId}/users")
+    suspend fun getRoomsUsers(
+        @Path("roomId") roomId: Int
+    ): BaseResponse<RoomsUsersResponse>
 }
