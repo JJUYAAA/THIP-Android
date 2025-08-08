5b3b0416cf58e0ac24e3c46005f2dbb65c8f871e
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-08-08 23:44:48 +0900

    [feat]: 기록장 조회 service 작성 (#71)

--- app/src/main/java/com/texthip/thip/data/service/RoomsService.kt
+++ app/src/main/java/com/texthip/thip/data/service/RoomsService.kt
@@ -2,9 +2,11 @@ package com.texthip.thip.data.service
 
 import com.texthip.thip.data.model.base.BaseResponse
 import com.texthip.thip.data.model.rooms.response.RoomsPlayingResponse
+import com.texthip.thip.data.model.rooms.response.RoomsPostsResponse
 import com.texthip.thip.data.model.rooms.response.RoomsUsersResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RoomsService {
     @GET("rooms/{roomId}/playing")
@@ -16,4 +18,16 @@ interface RoomsService {
     suspend fun getRoomsUsers(
         @Path("roomId") roomId: Int
     ): BaseResponse<RoomsUsersResponse>
+
+    @GET("rooms/{roomId}/posts")
+    suspend fun getRoomsPosts(
+        @Path("roomId") roomId: Int,
+        @Query("type") type: String = "group",
+        @Query("sort") sort: String? = "latest",
+        @Query("pageStart") pageStart: Int? = null,
+        @Query("pageEnd") pageEnd: Int? = null,
+        @Query("isOverview") isOverview: Boolean? = false,
+        @Query("isPageFilter") isPageFilter: Boolean? = false,
+        @Query("cursor") cursor: String? = null,
+    ): BaseResponse<RoomsPostsResponse>
 }
