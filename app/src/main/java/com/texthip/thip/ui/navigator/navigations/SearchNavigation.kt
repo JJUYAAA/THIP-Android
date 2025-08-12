022482259e4fad8d82c9d6fbb43022e04cdd46b2
rbqks529 <rbqks529@naver.com>
2025-08-12 21:44:19 +0900

    [feat]: 책 자세히 보기 네비게이션 연결 (#67)

--- app/src/main/java/com/texthip/thip/ui/navigator/navigations/SearchNavigation.kt
+++ app/src/main/java/com/texthip/thip/ui/navigator/navigations/SearchNavigation.kt
@@ -3,14 +3,43 @@ package com.texthip.thip.ui.navigator.navigations
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.texthip.thip.ui.navigator.extensions.navigateToBookDetail
 import com.texthip.thip.ui.navigator.routes.MainTabRoutes
+import com.texthip.thip.ui.navigator.routes.SearchRoutes
 import com.texthip.thip.ui.search.screen.SearchBookScreen
+import com.texthip.thip.ui.search.screen.SearchBookDetailScreen
 
 fun NavGraphBuilder.searchNavigation(navController: NavHostController) {
     composable<MainTabRoutes.Search> {
         SearchBookScreen(
-            navController = navController
-            // TODO: popularBooks를 서버에서 가져와서 전달
+            onBookClick = { isbn ->
+                navController.navigateToBookDetail(isbn)
+            }
+        )
+    }
+    
+    composable<SearchRoutes.BookDetail> { backStackEntry ->
+        val route = backStackEntry.toRoute<SearchRoutes.BookDetail>()
+        val isbn = route.isbn
+        
+        SearchBookDetailScreen(
+            isbn = isbn,
+            onLeftClick = {
+                navController.popBackStack()
+            },
+            onRightClick = {
+                // TODO: 우측 버튼 액션 구현
+            },
+            onRecruitingGroupClick = {
+                // TODO: 모집중인 그룹 화면으로 이동
+            },
+            onBookMarkClick = { isBookmarked ->
+                // TODO: 북마크 액션 구현
+            },
+            onWriteFeedClick = {
+                // TODO: 피드 작성 화면으로 이동
+            }
         )
     }
 }
