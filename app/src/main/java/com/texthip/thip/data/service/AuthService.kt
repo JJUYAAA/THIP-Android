e80c3a56058be7df10d5b41e09c267c8512f635e
JJUYAAA <jjjjyy020704@naver.com>
2025-08-12 18:23:00 +0900

    [feat]: 카카오 - AuthService 생성(#68)

---
+++ app/src/main/java/com/texthip/thip/data/service/AuthService.kt
@@ -0,0 +1,15 @@
+package com.texthip.thip.data.service
+
+import com.texthip.thip.data.model.auth.request.AuthRequest
+import com.texthip.thip.data.model.auth.response.AuthResponse
+import com.texthip.thip.data.model.base.BaseResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthService {
+    //신규유저 여부 확인
+    @POST("oauth2/users")
+    suspend fun checkNewUser(
+        @Body request: AuthRequest
+    ): BaseResponse<AuthResponse>
+}
