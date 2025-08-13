8ad2f23db66f20e62548fb7a4dfadf94d336f2c7
JJUYAAA <jjjjyy020704@naver.com>
2025-08-14 02:17:27 +0900

    [feat]: 레포지토리에 getMyPageInfo 생성(#73)

--- app/src/main/java/com/texthip/thip/data/repository/UserRepository.kt
+++ app/src/main/java/com/texthip/thip/data/repository/UserRepository.kt
@@ -2,6 +2,7 @@ package com.texthip.thip.data.repository
 
 import com.texthip.thip.data.model.base.handleBaseResponse
 import com.texthip.thip.data.model.users.MyFollowingsResponse
+import com.texthip.thip.data.model.users.MyPageInfoResponse
 import com.texthip.thip.data.model.users.OthersFollowersResponse
 import com.texthip.thip.data.service.UserService
 import javax.inject.Inject
@@ -11,6 +12,7 @@ import javax.inject.Singleton
 class UserRepository@Inject constructor(
     private val userService: UserService
 ) {
+    //내 팔로잉 목록 조회
     suspend fun getMyFollowings(
         cursor: String?,
         size: Int = 10
@@ -20,12 +22,20 @@ class UserRepository@Inject constructor(
             .getOrThrow()
     }
 
+    //다른 유저 팔로워 목록 조회
     suspend fun getOthersFollowers(
         userId: Long,
         cursor: String?,
         size: Int = 10
     ): Result<OthersFollowersResponse?> = runCatching {
         userService.getUserFollowers(userId = userId, cursor = cursor, size = size)
+            .handleBaseResponse()
+            .getOrThrow()
+    }
+
+    //마이페이지 정보 조회
+    suspend fun getMyPageInfo(): Result<MyPageInfoResponse?> = runCatching {
+        userService.getMyPage()
             .handleBaseResponse()
             .getOrThrow()
     }
