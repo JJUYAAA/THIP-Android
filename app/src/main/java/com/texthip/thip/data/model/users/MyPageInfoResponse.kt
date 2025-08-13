eccfb14c7c99c7fa27c8e95c270c796c49ae12b5
JJUYAAA <jjjjyy020704@naver.com>
2025-08-14 02:16:27 +0900

    [feat]: 마이페이지 정보 응답 dto(#73)

---
+++ app/src/main/java/com/texthip/thip/data/model/users/MyPageInfoResponse.kt
@@ -0,0 +1,12 @@
+package com.texthip.thip.data.model.users
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MyPageInfoResponse(
+    @SerialName("profileImageUrl") val profileImageUrl: String?,
+    @SerialName("nickname") val nickname: String,
+    @SerialName("aliasName") val aliasName: String,
+    @SerialName("aliasColor") val aliasColor: String
+)
