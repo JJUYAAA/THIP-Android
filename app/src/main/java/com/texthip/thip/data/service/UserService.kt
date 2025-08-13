a4f10c5a71177dcc05af99025776d540f9de8326
JJUYAAA <jjjjyy020704@naver.com>
2025-08-14 01:59:13 +0900

    [feat]: getUserFollowers 추가(#73)

--- app/src/main/java/com/texthip/thip/data/service/UserService.kt
+++ app/src/main/java/com/texthip/thip/data/service/UserService.kt
@@ -1,8 +1,8 @@
 package com.texthip.thip.data.service
 
 import com.texthip.thip.data.model.base.BaseResponse
-import com.texthip.thip.data.model.rooms.response.RoomsUsersResponse
 import com.texthip.thip.data.model.users.MyFollowingsResponse
+import com.texthip.thip.data.model.users.OthersFollowersResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -14,4 +14,10 @@ interface UserService {
         @Query("cursor") cursor: String? = null
     ): BaseResponse<MyFollowingsResponse>
 
+    @GET("users/{userId}/followers")
+    suspend fun getUserFollowers(
+        @Path("userId") userId: Long,
+        @Query("size") size: Int = 10,
+        @Query("cursor") cursor: String? = null
+    ): BaseResponse<OthersFollowersResponse>
 }
