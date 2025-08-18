975556566d1f66eab8758455df0dc08caed1fe81
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-08-18 22:39:10 +0900

    [refactor]: 댓글 바텀시트 높이 수정 (#101)

--- app/src/main/java/com/texthip/thip/ui/group/note/component/CommentBottomSheet.kt
+++ app/src/main/java/com/texthip/thip/ui/group/note/component/CommentBottomSheet.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -70,7 +69,7 @@ fun CommentBottomSheet(
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(600.dp)
+                    .fillMaxHeight(0.8f)
                     .advancedImePadding()
             ) {
                 Column(
