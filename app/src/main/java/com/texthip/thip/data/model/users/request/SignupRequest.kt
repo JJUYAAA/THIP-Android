d116345740d5df0978d9cbe21f18a58f728c790b
JJUYAAA <jjjjyy020704@naver.com>
2025-08-18 13:13:41 +0900

    [feat]: 회원가입 관련 dto 수정 (#73)

--- app/src/main/java/com/texthip/thip/data/model/users/request/SignupRequest.kt
+++ app/src/main/java/com/texthip/thip/data/model/users/request/SignupRequest.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SignupRequest(
-    val nickName: String,
+    val nickname: String,
     val aliasName: String
 )
--- app/src/main/java/com/texthip/thip/data/model/users/response/SignupResponse.kt
+++ app/src/main/java/com/texthip/thip/data/model/users/response/SignupResponse.kt
@@ -1,9 +1,10 @@
 package com.texthip.thip.data.model.users.response
 
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class SignupResponse(
-    val accessToken: String,
-    val refreshToken: String
+    @SerializedName("accessToken") val accessToken: String,
+    @SerializedName("userId") val userId: Long
 )
