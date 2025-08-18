14ec98c5133f6ea7b644ec7c06b7c1090ce53cce
JJUYAAA <jjjjyy020704@naver.com>
2025-08-18 22:13:12 +0900

    [feat]: 피드 좋아요 상태 변경 요청, 응답 dto (#96)

---
+++ app/src/main/java/com/texthip/thip/data/model/feed/request/FeedLikeRequest.kt
@@ -0,0 +1,9 @@
+package com.texthip.thip.data.model.feed.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class FeedLikeRequest(
+    @SerialName("type") val type: Boolean
+)
