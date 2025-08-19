619cae4b499ce373afdcf4e55b8dc5fe674aac02
rbqks529 <rbqks529@naver.com>
2025-08-19 22:40:17 +0900

    [refactor]: 피드 책 영역 부분 "저"가 항상 보이게 수정 (#102)

--- app/src/main/java/com/texthip/thip/ui/mypage/component/BookContent.kt
+++ app/src/main/java/com/texthip/thip/ui/mypage/component/BookContent.kt
@@ -2,13 +2,11 @@ package com.texthip.thip.ui.mypage.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
