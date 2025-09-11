e8141b0ea5cd3f00fa7d8fdbd32cb15aca78f83a
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-09-11 16:45:38 +0900

    [chore]: 배포 버전 업그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.texthip.thip"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.0"
+        versionCode = 7
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
