0c7a691b5009862758024e29ff421d73917b0f61
rbqks529 <rbqks529@naver.com>
2025-09-23 21:32:23 +0900

    [feat]: Common 네비게이션 수정 (#140)

--- app/src/main/java/com/texthip/thip/ui/navigator/navigations/CommonNavigation.kt
+++ app/src/main/java/com/texthip/thip/ui/navigator/navigations/CommonNavigation.kt
@@ -1,13 +1,9 @@
 package com.texthip.thip.ui.navigator.navigations
 
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import com.texthip.thip.ui.common.alarmpage.screen.AlarmScreen
-import com.texthip.thip.ui.common.alarmpage.viewmodel.AlarmViewModel
 import com.texthip.thip.ui.common.screen.RegisterBookScreen
 import com.texthip.thip.ui.navigator.routes.CommonRoutes
 
@@ -18,12 +14,7 @@ fun NavGraphBuilder.commonNavigation(
 ) {
     // Alarm 화면
     composable<CommonRoutes.Alarm> {
-        val alarmViewModel: AlarmViewModel = viewModel()
-        val alarmItems by alarmViewModel.alarmItems.collectAsState()
-        
         AlarmScreen(
-            alarmItems = alarmItems,
-            onCardClick = { alarmViewModel.onCardClick(it) },
             onNavigateBack = navigateBack
         )
     }
