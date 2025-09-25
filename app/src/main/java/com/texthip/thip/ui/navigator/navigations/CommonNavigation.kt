e74142c833ffebc101737bec158e18654cd62365
rbqks529 <rbqks529@naver.com>
2025-09-25 23:45:12 +0900

    [feat]: AlarmScreen의 Navigation 콜백 함수 추가(#140)

--- app/src/main/java/com/texthip/thip/ui/navigator/navigations/CommonNavigation.kt
+++ app/src/main/java/com/texthip/thip/ui/navigator/navigations/CommonNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import com.texthip.thip.ui.common.alarmpage.screen.AlarmScreen
 import com.texthip.thip.ui.common.screen.RegisterBookScreen
+import com.texthip.thip.ui.navigator.extensions.navigateFromNotification
 import com.texthip.thip.ui.navigator.routes.CommonRoutes
 
 // Common 관련 네비게이션
@@ -15,10 +16,13 @@ fun NavGraphBuilder.commonNavigation(
     // Alarm 화면
     composable<CommonRoutes.Alarm> {
         AlarmScreen(
-            onNavigateBack = navigateBack
+            onNavigateBack = navigateBack,
+            onNotificationNavigation = { response ->
+                navController.navigateFromNotification(response)
+            }
         )
     }
-    
+
     // 책 요청 화면
     composable<CommonRoutes.RegisterBook> {
         RegisterBookScreen(
