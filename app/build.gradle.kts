7a096051b67e6eebc4eb036929501caa7aa4a7ec
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2025-09-26 14:22:48 +0900

    [chore]: 배포 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.texthip.thip"
         minSdk = 29
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.2.1"
+        versionCode = 8
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
